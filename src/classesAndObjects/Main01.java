49a417611737f0df7499cb957a54d69e0d86a380
kiyodesu <kiyotomm99@gmail.com>
2024-11-01 01:24:46 +0900

    classes and objects

---
+++ src/classesAndObjects/Main01.java
@@ -0,0 +1,11 @@
+package classesAndObject;
+public class Main01 {
+    public static void main(String[] args) {
+        var textBox1 = new TextBox();
+        textBox1.setText("Box 1");
+        System.out.println(textBox1.text);
+
+        var textBox2 = new textBox();
+
+    }
+}
